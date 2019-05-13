e100ce54baf451d3ae275f49289945e503354f9b
Felix Klement <mail@felix-klement.de>
2019-05-13 14:30:33 +0200

    Add prepared statement to update + add emulation false for pdo

--- part_1/www/login_prepared_statement.php
+++ part_1/www/login_prepared_statement.php
@@ -9,12 +9,13 @@ session_name($htbconf['bank/sid']);
 session_start();
 
 set_error_handler('errorHandler');
-$link = new mysqli($htbconf['db/.server'], $htbconf['db/.login'], $htbconf['db/.pwd'], $htbconf['db/.name']) or die('Error connecting to database');
 
 $username = $_REQUEST['username'];
 $password = $_REQUEST['password'];
 $hostname = $_SERVER['HTTP_HOST'];
-$sql = "SELECT * FROM " . $htbconf['db/users'] . " where " . $htbconf['db/users.username'] . "='$username' and " . $htbconf['db/users.password'] . "='$password'";
+
+$link = new mysqli($htbconf['db/.server'], $htbconf['db/.login'], $htbconf['db/.pwd'], $htbconf['db/.name']) or die('Error connecting to database');
+$link->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
 // Prepares the SQL statement for execution
 $stmt = $link->prepare("SELECT * FROM " . $htbconf['db/users'] . " WHERE " . $htbconf['db/users.username'] . "= ? AND " . $htbconf['db/users.password'] . "= ?");
@@ -36,7 +37,11 @@ if ($stmt->execute()) {
 			$_SESSION['time'] = strtotime($row[5]);
 			$_SESSION['lastlogin'] = strtotime($row[6]);
 			$_SESSION['lastloginip'] = $row[7];
-			$sql = "UPDATE " . $htbconf['db/users'] . " set " . $htbconf['db/users.lasttime'] . "=now(), " . $htbconf['db/users.lastip'] . "='" . $_SERVER['REMOTE_ADDR'] . "' where " . $htbconf['db/users.username'] . "='$username' and " . $htbconf['db/users.password'] . "='$password'";
+			
+			// Prepares the SQL statement for execution
+			$stmt = $link->prepare("UPDATE " . $htbconf['db/users'] . " set " . $htbconf['db/users.lasttime'] . "=now(), " . $htbconf['db/users.lastip'] . "='" . $_SERVER['REMOTE_ADDR'] . "' where " . $htbconf['db/users.username'] . "=? and " . $htbconf['db/users.password'] . "=?");
+			// Binds the username and the password to the prepared statement as string parameters
+			$stmt->bind_param('ss', $username, $password);
 			if (!$stmt->execute()) $_SESSION['warning'].= "<p>Unable to update login time and login ip.</p><p>Please report this to your system administrator.</p>";
 			htb_redirect(htb_pageurl("htbmain"));
 		} else {
